16421aa0a3a0ef5b05ff6ba8586a341016b84f1d
tyrannosaurus-lee <miaow106@gmail.com>
2023-10-02 19:34:15 +0900

    4.9 Props 사용해보기 - props.image, props.name, props.title 이런식으로 사용해보기(오류수정)

--- chapter4/basic/src/components/Profile.jsx
+++ chapter4/basic/src/components/Profile.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 // props : 외부로부터 주어진 props를 이용해서 텍스트를 보여줌
-export default function Profile({props}) {
+export default function Profile(props) {
     return (
         <div className='profile'>
             <img className='photo' src={props.image} alt='avatar' />
             <h1>{props.name}</h1>
             <p>{props.title}</p>
         </div>
-    )
+    );
 }
