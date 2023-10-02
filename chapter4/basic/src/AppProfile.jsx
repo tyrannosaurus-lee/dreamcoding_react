f7965fd13a8f7d4b60d7142625d169bd24667377
tyrannosaurus-lee <miaow106@gmail.com>
2023-10-02 22:06:15 +0900

    4.13 아바타 컴포넌트 만들기 - 아바타 부분만 사용하기

--- chapter4/basic/src/AppProfile.jsx
+++ chapter4/basic/src/AppProfile.jsx
@@ -1,13 +1,19 @@
 import './App.css';
+import Avatar from './components/Avatar';
 import Profile from './components/Profile';
 
 function AppProfile() {
     return (
         <>
+            <Avatar
+                image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-'
+                isNew={true}
+            />
             <Profile
                 image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-'
                 name='James Kim'
                 title='프론트엔드 개발자'
+                isNew={true}
             />
             <Profile
                 image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
--- chapter4/basic/src/components/Profile.jsx
+++ chapter4/basic/src/components/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Avatar from './Avatar';
 
 // props : 외부로부터 주어진 props를 이용해서 텍스트를 보여줌
-export default function Profile({image, name, title}) {
+export default function Profile({ image, name, title, isNew }) {
     return (
         <div className='profile'>
-            <img className='photo' src={image} alt='avatar' />
+            <Avatar image={image} isNew={isNew} />
             <h1>{name}</h1>
             <p>{title}</p>
         </div>
