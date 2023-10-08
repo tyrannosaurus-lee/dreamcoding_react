bef85cee216d819f2ce995244002c336f02a543c
tyrannosaurus-lee <miaow106@gmail.com>
2023-10-08 12:36:51 +0900

    4.15 내부 상태관리 State - 정적 마크업

--- chapter4/basic/src/index.js
+++ chapter4/basic/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppProfile from './AppProfile';
+import AppCounter from './AppCounter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <AppProfile />
+    {/* <AppProfile /> */}
+    <AppCounter />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
