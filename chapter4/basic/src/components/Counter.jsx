09e530ad4a506b4d83e764c92b797d5cd0bf5034
tyrannosaurus-lee <miaow106@gmail.com>
2023-10-08 19:41:04 +0900

    4.16 useState 유의할점! - useState와 콜백함수 사용하기

--- chapter4/basic/src/components/Counter.jsx
+++ chapter4/basic/src/components/Counter.jsx
@@ -17,15 +17,26 @@ export default function Counter() {
             <button
                 className='button'
                 onClick={()=> {
-                    setCount(count + 1); // 0 + 1
-                    setCount(count + 1); // 0 + 1
-                    setCount(count + 1); // 0 + 1
-                    setCount(count + 1); // 0 + 1
-                    setCount(count + 1); // 0 + 1
+                    setCount((prev)=> {
+                        return prev + 1
+                    });
+                    setCount((prev)=> prev + 1);
+                    setCount((prev)=> prev + 1);
+                    setCount((prev)=> prev + 1);
+                    setCount((prev)=> prev + 1);
                 }}
-                // 숫자는 5가 아닌 1이 나옴
-                // onClick이 호출되었을 때 전달되는 콜백함수는 현재 상태를 스냅샷으로 저장함.
-                // 아무리 많이 setCount호출을 해도 count가 계속 0으로 고정되어 있어서 1로 설정됨
+                // 함수가 전달될때 스냅샷됐을때
+                // 내부에서 외부에 참조하고 있는게 아무것도 없음.
+                // 그래서 setCount호출될때 이전상태값(prev)을 콜백인자로 전달받는데 리액트가 전달해줌
+                // 결론 : 리액트에서 상태를 사용할때는 그냥 일반변수를 쓰면 안되고
+                // 리액트에서 제공하는 useState를 써야함
+                // 그래서 useState초기값을 설정해주고 useState를 호출하면 배열이 전달되는데
+                // 배열의 첫번째는 값을 가리키고 있는 변수
+                // 두번째는 이것을 업데이트 할 수 있는 변수이다.
+                // 업데이트 하는 함수는 바로 우리가 값을 설정해서 setCount는 0, 1..이렇게 지정해도 되고
+                // 여러번 지정해야하거나 이전 상태값을 토대로 더하거나 빼거나 이전 상태값을 업데이트하는 경우에는
+                // 기존의 스냅샷된 외부값에 의존하기 보다는
+                // 콜백함수 형태로 set하는것이 더 안전하다.
             >
                 Add +
             </button>
